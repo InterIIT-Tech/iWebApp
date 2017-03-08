d2d0be864061c7ef2120501a69edf4fd9904b0c1
muks14x <muks14x@gmail.com>
2017-03-09 03:35:26 +0530

    Add Clubs page

--- render/clubs.php
+++ render/clubs.php
@@ -45,281 +45,49 @@
 						</div>
 					</section>
 				<!-- Computer Science Header -->
-					<section id="cse" class="wrapper style3 fade-up">
+					<section id="cse" class="wrapper style2 fade-up">
 						<div class="inner">
 							<h2>NJACK</h2>
 							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<ul class="actions">
+										<li><a href="#" class="button">Subscribe</a></li>
+							</ul>
 						</div>
 					</section>
 
-				<!-- Computer Science Course List -->
-					<section class="wrapper style2 spotlights">
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic03.jpg&quot;); background-position: 25% 25%;"><img src="img/courses/pic03.jpg" alt="" data-position="25% 25%" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Ultricies aliquam</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-					<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section><section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section></section>
+				
 				<!-- Electrical Header -->
-					<section id="ee" class="wrapper style3 fade-up">
+					<section id="ee" class="wrapper style2 fade-up">
 						<div class="inner">
 							<h2>Sparkonics</h2>
 							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<ul class="actions">
+										<li><a href="#" class="button">Subscribe</a></li>
+							</ul>
 						</div>
 					</section>
 
-				<!-- Electrical Course List -->
-					<section class="wrapper style2 spotlights">
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic03.jpg&quot;); background-position: 25% 25%;"><img src="img/courses/pic03.jpg" alt="" data-position="25% 25%" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Ultricies aliquam</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-					<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section><section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section></section>
 				<!-- Mechanical Header -->
-					<section id="me" class="wrapper style3 fade-up">
+					<section id="me" class="wrapper style2 fade-up">
 						<div class="inner">
 							<h2>SCME</h2>
 							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<ul class="actions">
+										<li><a href="#" class="button">Subscribe</a></li>
+							</ul>
 						</div>
 					</section>
 
-				<!-- Mechanical Course List -->
-					<section class="wrapper style2 spotlights">
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic03.jpg&quot;); background-position: 25% 25%;"><img src="img/courses/pic03.jpg" alt="" data-position="25% 25%" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Ultricies aliquam</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-					<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section><section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section></section>
 				<!-- E-Club Header -->
-				<section id="ce" class="wrapper style3 fade-up">
+				<section id="ce" class="wrapper style2 fade-up">
 						<div class="inner">
 							<h2>E-Club</h2>
 							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<ul class="actions">
+										<li><a href="#" class="button">Subscribe</a></li>
+							</ul>
 						</div>
 					</section>
-
-				<!-- E-Club Course List -->
-					<section class="wrapper style2 spotlights">
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic03.jpg&quot;); background-position: 25% 25%;"><img src="img/courses/pic03.jpg" alt="" data-position="25% 25%" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Ultricies aliquam</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-					<section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic01.jpg&quot;); background-position: center center;"><img src="img/courses/pic01.jpg" alt="" data-position="center center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section><section class="">
-							<a href="#" class="image" style="background-image: url(&quot;img/courses/pic02.jpg&quot;); background-position: center top;"><img src="img/courses/pic02.jpg" alt="" data-position="top center" style="display: none;"></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Subscribe</a></li>
-									</ul>
-								</div>
-							</div>
-						</section></section>
 			</div>
 
 		<!-- Scripts -->

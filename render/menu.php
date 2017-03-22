c907538f6c8e35e30948b731c882f5036173cbd1
Mukuntha N S <muks14x@gmail.com>
2017-03-22 19:36:58 +0530

    Update menu bar

--- render/menu.php
+++ render/menu.php
@@ -1,12 +1,12 @@
 <?php
-echo '<nav class="outer-nav right vertical">
-				<a href="#" class="icon-home">Home</a>
- 				<a href="#" class="icon-news">News</a>
- 				<a href="#" class="icon-image">Images</a>
- 				<a href="#" class="icon-upload">Uploads</a>
- 				<a href="#" class="icon-star">Favorites</a>
- 				<a href="#" class="icon-mail">Messages</a>
- 				<a href="#" class="icon-lock">Security</a>
+echo '<nav class="outer-nav right vertical" style="font-size: 150%;" >
+				<a href="." class="icon-home">Home</a>
+ 				<a href="timetable" class="icon-news">Timetable</a>
+				<a href="courses"><i class="fa fa-book" aria-hidden="true"></i> Courses</a>
+ 				<a href="clubs" class="icon-star">Clubs</a>
+ 				<a href="gallery" class="icon-image">Gallery</a> 
+ 				<a href="getting-around"><i class="fa fa-map" aria-hidden="true"></i> Getting Around Campus</a>
+ 				<a href="lost-found"><i class="fa fa-archive" aria-hidden="true"></i> Lost and Found</a>
  	</nav>
 ';
 ?>

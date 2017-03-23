04703d117ad8ed0cee5e537c24df6806bdb6862c
tameeshb <tameeshbiswas1998@gmail.com>
2017-03-23 21:04:19 +0530

    Map temp fix

--- render/maps.php
+++ render/maps.php
@@ -9,25 +9,7 @@ if($result && mysqli_num_rows($result)>0){
 		$list.= "<option pxcoord='".$row['pX']."' pycoord='".$row['pY']."' style='background-color: #2a2f4a ;'>".$row['pName']."</option>";
 		
 	}
-															            }
-function GetDrivingDistance($lat1, $lat2, $long1, $long2){
-    $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$lat1.",".$long1."&destinations=".$lat2.",".$long2."&mode=driving&language=pl-PL&key=AIzaSyCDoS4zxUVccoOIkEZadbmTssHyiV__QXw";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    $response_a = json_decode($response, true);
-    print_r($response_a);
-    $dist = $response_a['rows'][0]['elements'][0]['distance']['text'];
-    $time = $response_a['rows'][0]['elements'][0]['duration']['text'];
-
-    return array('distance' => $dist, 'time' => $time);
-}
-print_r(GetDrivingDistance(25.533542,25.540570,84.855469,84.851472));
+		
 ?>
 <!DOCTYPE HTML>
 <html>
